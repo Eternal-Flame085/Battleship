7cacaba5d6e4f186d0e9eb99c43a0f8d57fda58f
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-25 11:43:43 -0400

    Cell variables are readable

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/cell"
-require "./lib/ship"
+#require "./lib/ship"
 
 class CellTest < Minitest::Test
 
@@ -9,6 +9,13 @@ class CellTest < Minitest::Test
     cell = Cell.new("B4")
 
     assert_instance_of Cell, cell
+  end
+
+  def  test_checks_variables_are_readable
+     cell = Cell.new("B4")
+
+     assert_equal "B4", cell.coordinate
+     assert_nil cell.ship
   end
 
 end
