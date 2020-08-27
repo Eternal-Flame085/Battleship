c8451af38e2b095c1d3a510cedb7c9a55f1d5e7f
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-08-27 00:29:34 -0600

    Created validation method and tests within board class for both horizontal and vertical placements with all tests passing

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,6 +2,7 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/board"
 require "./lib/cell"
+require "./lib/ship"
 
 class BoardTest < Minitest::Test
   def test_it_exists
@@ -27,16 +28,27 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?("E1")
     assert_equal false, board.valid_coordinate?("A22")
   end
-def test_valid_placements
+def test_length_placements
+
   board = Board.new
   cruiser = Ship.new("Cruiser", 3)
   submarine = Ship.new("Submarine", 2)
 
   assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
   assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+
+  end
+
+def test_consecutive_placement
+
+  board = Board.new
+  cruiser = Ship.new("Cruiser", 3)
+  submarine = Ship.new("Submarine", 2)
+
   assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
   assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
   assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
   assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
   end
 end
