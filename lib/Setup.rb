2fa5db5357ab0365086b48818d3b566da7f91195
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-08-28 20:02:42 -0600

    Created the computer ship placement AI for the setup class so computer randomly places ship with valid coordinates

--- lib/Setup.rb
+++ lib/Setup.rb
@@ -1,6 +1,6 @@
 class Setup
   def initialize
-    main_menu
+
   end
 
   def main_menu
@@ -14,6 +14,33 @@ class Setup
     exit if user_input == "q"
     if user_input == p
       play_game
+    end
+  end
+
+  def play_game
+    computer_ship_placement
+    require "pry";binding.pry
+  end
+
+  def computer_ship_placement
+    randomizer = rand(2)
+    if randomizer == 0
+      letter = ("A".."D").to_a.sample
+      collector_for_each_cons = []
+      horizontal_computer_placement = ("#{letter}1".."#{letter}4")
+      horizontal_computer_placement.each_cons(3){|consecutive_numbers| collector_for_each_cons << consecutive_numbers}
+      return collector_for_each_cons[rand(collector_for_each_cons.length)]
+    elsif randomizer == 1
+      number = ("1".."4").to_a.sample
+      collector_for_each_cons = []
+      vertical_computer_placement = ("A".."D")
+      vertical_computer_placement.each_cons(3){|consecutive_letters| collector_for_each_cons << consecutive_letters}
+      letter_coordinates = collector_for_each_cons[rand(collector_for_each_cons.length)]
+
+      letter_coordinates.length.times do |counter|
+        letter_coordinates[counter].insert(-1,number)
+      end
+      return letter_coordinates
     end
   end
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,5 @@
 require "./lib/setup"
 
 setup = Setup.new
+
+setup.play_game
